6c2b81b200427a01ca6b16d6f8a7df6676d511d4
reza <rdezhhoot@gmail.com>
2022-07-23 16:55:36 +0430

    mofify theme lvl 3

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -32,7 +32,7 @@
                 <!--begin::Entry-->
                 <div class="d-flex flex-column-fluid">
                     <!--begin::Container-->
-                    <div class="container">
+                    <div class="container-fluid">
                         @yield('content')
                     </div>
                     <!--end::Container-->
