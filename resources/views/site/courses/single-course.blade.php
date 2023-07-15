868294ab49f460e3355a43f94999b5b56454453b
Reza Dezhhoot <rdezhhoot@gmail.com>
2023-07-15 10:50:57 +0330

    new update

--- resources/views/site/courses/single-course.blade.php
+++ resources/views/site/courses/single-course.blade.php
@@ -436,7 +436,7 @@
                                 </li>
                                 <li class="d-flex align-items-center justify-content-between">
                                     <span><i class="la la-bolt mr-2 text-color"></i>امتحان</span>
-                                    @if(is_null($course->quiz))
+                                    @if(!is_null($course->quiz))
                                         <a href="{{route('sandbox')}}">بله (شرکت در آزمون آزمایشی)</a>
                                     @else
                                         خیر
