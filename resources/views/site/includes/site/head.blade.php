9bf89a2cad7adc78208d3ef5e549d59779c84be9
Reza Dezhhoot <rdezhhoot@gmail.com>
2024-05-30 13:06:42 +0330

    update code

--- resources/views/site/includes/site/head.blade.php
+++ resources/views/site/includes/site/head.blade.php
@@ -3,12 +3,13 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="canonical" href="{{ url()->current() }}"> />
+
     @stack('head')
     {!! SEOMeta::generate() !!}
     {!! OpenGraph::generate() !!}
     {!! Twitter::generate() !!}
     {!! JsonLd::generate() !!}
+    <link rel="canonical" href="{{ url()->current() }}" />
     <!-- Google fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com/" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&amp;display=swap" rel="stylesheet" />
--- resources/views/site/layouts/site/site.blade.php
+++ resources/views/site/layouts/site/site.blade.php
@@ -2,13 +2,6 @@
 <html lang="fa">
     <livewire:site.includes.site.head/>
     <body>
-{{--        <div class="preloader">--}}
-{{--            <div class="loader">--}}
-{{--                <svg class="spinner" viewBox="0 0 50 50">--}}
-{{--                    <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>--}}
-{{--                </svg>--}}
-{{--            </div>--}}
-{{--        </div>--}}
         <livewire:site.includes.site.header/>
         @yield('content')
         <livewire:site.includes.site.footer/>
