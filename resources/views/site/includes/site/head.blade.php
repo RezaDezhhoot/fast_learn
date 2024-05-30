1ae9427125882b2ec83d4c9db68468885d7c2023
Reza Dezhhoot <rdezhhoot@gmail.com>
2024-05-30 12:42:39 +0330

    update code

--- resources/views/site/includes/site/head.blade.php
+++ resources/views/site/includes/site/head.blade.php
@@ -3,6 +3,7 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="canonical" href="{{ url()->current() }}"> />
     @stack('head')
     {!! SEOMeta::generate() !!}
     {!! OpenGraph::generate() !!}
